a81204ceed82e927247881089d46f914c31e6786
Joel Lafond-Lapalme <joel.lafond-lapalme@agr.gc.ca>
2022-10-20 09:31:10 -0400

    Add comments to script

--- iris_analysis.R
+++ iris_analysis.R
@@ -1,12 +1,16 @@
+# load packages
 library(tidyverse)
 
+# load iris dataset
 data(iris)
 
+# Table: mean sepal length by species
 iris %>%
   as_tibble() %>%
   group_by(Species)%>%
   summarize(mean_sepeal_length = mean(Sepal.Length))
 
+# Boxplot sepal length by species
 iris %>%
   as_tibble() %>%
   ggplot(aes(x = Species, y = Sepal.Length))+
