585b68373f4d34f2927d374e687c69c7f775c2b4
Joel Lafond-Lapalme <joel.lafond-lapalme@agr.gc.ca>
2022-10-20 10:03:07 -0400

    Revert "messed up script"
    This reverts commit 89856bfdb0406ae1ffda03a2768a393a6ec8299c.

--- iris_analysis.R
+++ iris_analysis.R
@@ -3,24 +3,18 @@ library(tidyverse)
 
 # load iris dataset
 data(iris)
-fdsasdf
-group_by(Species)%>%
+
 # Table: mean sepal length by species
 iris %>%
-#1335
-#1435
-fd24325as
-  as_#454tibble() dsfa%>%
-  56
-  su654675mmarize(fdasdfa = mean(Sepal.Length))
-7657
-# Bo65986xplot sasdfdepal length by species
+  as_tibble() %>%
+  group_by(Species)%>%
+  summarize(mean_sepeal_length = mean(Sepal.Length))
+
+# Boxplot sepal length by species
 iris %>%
-fdasadsfdadfadsad
-fdaadf
   as_tibble() %>%
   ggplot(aes(x = Species, y = Sepal.Length))+
-  geom_boxplot(oudfdsadstlier.shape = NA)+
-  geom_jitter(width = 0.2, fdsafsize = 1)+
-  labs(x = "Iradsfdsais species", y = "Sepal Length")
-f
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(width = 0.2, size = 1)+
+  labs(x = "Iris species", y = "Sepal Length")
+
